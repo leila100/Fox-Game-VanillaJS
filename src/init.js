0c185e42c96f4a20179fe777eda2948309c69737
Leila <nb.leila10@gmail.com>
2020-04-26 15:34:31 -0500

    Add init.js that controls the time for the game

---
+++ src/init.js
@@ -0,0 +1,32 @@
+/*
+This is a technique where we can say to the browser: 
+  "Hey! Browser! Next time you have an idle moment, call me 😉" complete with winky face. 
+As you may imagine, this can be very frequent, and we don't want our browser to execute at lightspeed. 
+So in this case, we're saying, "check to see if it's been 3 seconds. If it has, call tick. If not, chill and wait for the next frame. 
+This way, we're guaranteed to run tick every three seconds, give or take a few milliseconds 
+(since the browser will wait until it's idle to call.)
+*/
+
+const TICK_RATE = 3000;
+
+function tick() {
+  console.log("tick", Date.now());
+}
+
+async function init() {
+  console.log("starting game");
+
+  let nextTimeToTick = Date.now();
+  function nextAnimationFrame() {
+    const now = Date.now();
+    if (nextTimeToTick <= now) {
+      tick();
+      nextTimeToTick = now + TICK_RATE;
+    }
+    requestAnimationFrame(nextAnimationFrame);
+  }
+
+  nextAnimationFrame();
+}
+
+init();
