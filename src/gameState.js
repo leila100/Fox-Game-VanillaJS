1ed0a097ef114ad8e90015a1c8940ce1af5aad9c
Leila <nb.leila10@gmail.com>
2020-04-26 16:29:27 -0500

    Setup the starGame and the wake functions

--- src/gameState.js
+++ src/gameState.js
@@ -1,10 +1,22 @@
 const gameState = {
   current: "INIT",
   clock: 1,
+  wakeTime: -1,
   tick() {
     this.clock++;
     console.log(this.clock);
+    if (this.clock === this.wakeTime) this.wake();
     return this.clock;
+  },
+  startGame() {
+    console.log("Hatching");
+    this.current = "HATCHING";
+    this.wakeTime = this.wakeTime + 3;
+  },
+  wake() {
+    console.log("hatched");
+    this.current = "IDLING";
+    this.wakeTime = -1;
   },
   handleUserAction(icon) {
     console.log(icon);
